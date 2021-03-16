14fa742f2b559e9417ea6051b2ffb5a5de002ef1
Paul <5748299+biggestcookie@users.noreply.github.com>
2021-03-16 03:43:52 +0000

    Replace deprecated alias with resolve.alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    "/@": path.resolve(__dirname, "./src"),
+  resolve: {
+    alias: {
+      "/@": path.resolve(__dirname, "./src"),
+    },
   },
   css: {
     preprocessorOptions: {
