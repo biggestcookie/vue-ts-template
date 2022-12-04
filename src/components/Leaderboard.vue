941c844f9a9c8316e396a377efa6fee483cb81cf
McLayn <mclayn.v@gmail.com>
2022-12-04 20:28:47 +0100

    Fix Leaderboard nulls
    Fix Leaderboard nulls

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -41,7 +41,7 @@ function msToElapsedString(totalms: number): string {
   <div className="content has-text-centered">
     <div className="block">
       <h2 className="title">{{ username }}&apos;s time:</h2>
-      <p className="subtitle">
+      <p className="subtitle" v-if="store.submittedRun">
         {{ msToElapsedString(store.submittedRun?.totalTimeMilliseconds) }}
       </p>
     </div>
@@ -59,7 +59,7 @@ function msToElapsedString(totalms: number): string {
           <tr
             v-for="(result, index) in store.topResults"
             :key="index"
-            :class="store.submittedRun.id === result.id ? 'is-selected' : ''"
+            :class="store.submittedRun?.id === result.id ? 'is-selected' : ''"
           >
             <th>{{ index + 1 }}</th>
             <td>{{ result.username }}</td>
@@ -82,7 +82,7 @@ function msToElapsedString(totalms: number): string {
           <tr
             v-for="(result, index) in store.userResults"
             :key="index"
-            :class="store.submittedRun.id === result.id ? 'is-selected' : ''"
+            :class="store.submittedRun?.id === result.id ? 'is-selected' : ''"
           >
             <th>{{ index + 1 }}</th>
             <td>{{ result.username }}</td>
